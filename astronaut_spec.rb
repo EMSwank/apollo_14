7ec6823638c387bd282e5f637bfd3690a4ea5ecd
Eliot Swank <eliotswank@gmail.com>
2018-07-09 08:30:19 -0600

    adds model test for average_age

---
+++ astronaut_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Astronaut do
+  describe 'instance methods' do
+    it ".average_age" do
+      astronaut1 = Astronaut.create(name: "Neil Armstrong", age: 32, job: "commander")
+      astronaut2 = Astronaut.create(name: "Buzz Aldren", age: 30, job: "commander")
+      avg_age = 31
+
+      expect(Astronaut.average_age).to eq(avg_age)
+    end
+    
+  end
+end
