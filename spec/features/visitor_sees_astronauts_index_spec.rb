56666b12cfed48c625b788be7c25cac4e92ae480
Eliot Swank <eliotswank@gmail.com>
2018-07-09 08:24:47 -0600

    adds feature test for user story 2

--- spec/features/visitor_sees_astronauts_index_spec.rb
+++ spec/features/visitor_sees_astronauts_index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "Visitor" do
   context "visits astronauts index" do
-    it "list of astronauts with name, age, job" do
+    it "sees list of astronauts with name, age, job" do
       astronaut = Astronaut.create(name: "Neil Armstrong", age: 37, job: "commander")
 
       visit astronauts_path
@@ -11,17 +11,20 @@ describe "Visitor" do
       expect(page).to have_content(astronaut.age)
       expect(page).to have_content(astronaut.job)
     end
+    it "sees the average age of all astronauts" do
+      astronaut1 = Astronaut.create(name: "Neil Armstrong", age: 32, job: "commander")
+      astronaut2 = Astronaut.create(name: "Buzz Aldren", age: 30, job: "commander")
+      avg_age = 31
+
+      visit astronauts_path
+
+      expect(page).to have_content("Average Age: #{avg_age}")
+    end
   end
 end
 
 =begin
-As a visitor,
-When I visit '/astronauts'
-I see a list of astronauts with the following info:
- - Name
- - Age
- - Job
- (e.g. "Name: Neil Armstrong" Age: 37 Job: Commander")
+
 ```
 
 ```
